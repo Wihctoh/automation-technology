08ffbec43e3de15f3ed2039070dbe2b19fc1ac5a
wihctoh <rivertoxi911@gmail.com>
2023-04-06 21:51:34 +0300

    fix

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-const dropdownListElText = document.querySelectorAll(".dropdown_list_el_text");
-const dropdownBtn = document.querySelectorAll(".dropdown_list_img");
+const container = document.querySelector(".dropdown_list");
+const dropdownListElText = document.querySelector(".dropdown_list_el_text");
+const dropdownBtn = document.querySelector(".dropdown_list_img");
 
-dropdownBtn[0].addEventListener("click", function () {
-  if (dropdownListElText[0].hidden) {
-    dropdownListElText[0].hidden = false;
-  } else dropdownListElText[0].hidden = true;
+container.addEventListener("click", function (event) {
+  event.target.appendChild(dropdownListElText);
 });
 
-dropdownBtn[1].addEventListener("click", function () {
-  if (dropdownListElText[1].hidden) {
-    dropdownListElText[1].hidden = false;
-  } else dropdownListElText[1].hidden = true;
-});
+// dropdownBtn[0].addEventListener("click", function () {
+//   if (dropdownListElText[0].hidden) {
+//     dropdownListElText[0].hidden = false;
+//   } else dropdownListElText[0].hidden = true;
+// });
 
-dropdownBtn[2].addEventListener("click", function () {
-  if (dropdownListElText[2].hidden) {
-    dropdownListElText[2].hidden = false;
-  } else dropdownListElText[2].hidden = true;
-});
+// dropdownBtn[1].addEventListener("click", function () {
+//   if (dropdownListElText[1].hidden) {
+//     dropdownListElText[1].hidden = false;
+//   } else dropdownListElText[1].hidden = true;
+// });
 
-dropdownBtn[3].addEventListener("click", function () {
-  if (dropdownListElText[3].hidden) {
-    dropdownListElText[3].hidden = false;
-  } else dropdownListElText[3].hidden = true;
-});
+// dropdownBtn[2].addEventListener("click", function () {
+//   if (dropdownListElText[2].hidden) {
+//     dropdownListElText[2].hidden = false;
+//   } else dropdownListElText[2].hidden = true;
+// });
+
+// dropdownBtn[3].addEventListener("click", function () {
+//   if (dropdownListElText[3].hidden) {
+//     dropdownListElText[3].hidden = false;
+//   } else dropdownListElText[3].hidden = true;
+// }
+// );
